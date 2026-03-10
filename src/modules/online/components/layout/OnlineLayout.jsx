f75ad1a6b89e6c44e0e5f48701c5e57fb960dc67
Victor-Armas <victor.961004@outlook.com>
2026-03-10 14:51:20 -0600

    fix: ThemeContext case sensitive

--- src/modules/online/components/layout/OnlineLayout.jsx
+++ src/modules/online/components/layout/OnlineLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import { ThemeProvider } from "../../context/ThemeContext.jsx";
-import { CartProvider } from "../../context/CartContext.jsx";
+import { ThemeProvider } from "../../context/ThemeContext";
+import { CartProvider } from "../../context/CartContext";
 
 export default function OnlineLayout() {
   return (
